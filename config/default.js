6b4262cd87f64f0c035afeb10b9eb446c86869bb
Adrien Risser <risseraka@gmail.com>
2013-12-23 00:33:02 +0100

    moving configuration into config files

---
+++ config/default.js
@@ -0,0 +1,16 @@
+module.exports = {
+    facebook: {
+	clientId: '92601131724',
+	clientSecret: 'd14f54eb8013c4604687225fc905e160'
+    },
+
+    twitter: {
+        consumerKey: 'XDhPOfEgvAfzFUPUWHhQ',
+        consumerSecret: 'dUT9wdqV0neYu93qsQE79Ny8JzY6blK8BoVb1mfeDBM'
+    },
+
+    google: {
+	clientId: '410681330301.apps.googleusercontent.com',
+	clientSecret: 'I2xgEom_U-mMvBRCBCzQ3W5I'
+    }
+};
